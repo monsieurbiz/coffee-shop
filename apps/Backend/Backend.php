9cbdefcc4f3bbf349d49b2d13096a7b50df44f55
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2016-05-16 23:26:13 +0200

    Init DB

--- apps/Backend/Backend.php
+++ apps/Backend/Backend.php
@@ -49,7 +49,7 @@ class Backend
             mkdir($dbDir, 0755);
         }
 
-        $this->_db = new \SQLite3($dbDir . '/coffee.db');
+        $this->_db = new \SQLite3($dbDir . '/coffee.sqlite');
 
         $result = $this->_db->query("SELECT name FROM sqlite_master WHERE type='item'");
 
@@ -60,7 +60,53 @@ class Backend
 
     public function installDb()
     {
-        //
+        // Lists
+        $time = time();
+        $this->_db->query("CREATE TABLE list (list_id INTEGER PRIMARY KEY AUTOINCREMENT, who VARCHAR(30), created_at INTEGER, shipping_at INTEGER, active INTEGER(1))");
+        $this->_db->query("INSERT INTO list (who, created_at, active) VALUES ('Monsieur Biz', $time, 1);");
+
+        // List items
+        $this->_db->query("CREATE TABLE list_item (item_id INTEGER, list_id INTEGER, qty INTEGER)");
+
+        // Items
+        $this->_db->query("CREATE TABLE item (item_id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(120), image VARCHAR(255))");
+
+        $items = [
+            # Intenso
+            ["Kazaar", "/ecom/medias/sys_master/public/9375751012382/C-0089-small-60x60.png", 12],
+            ["Dharkan", "/ecom/medias/sys_master/public/9375751602206/C-0090-small-60x60.png", 11],
+            ["Ristretto", "/ecom/medias/sys_master/public/9375746555934/C-0023-small-60x60.png", 10],
+            ["Arpeggio", "/ecom/medias/sys_master/public/9375741706270/C-0001-small-60x60.png", 9],
+            ["Roma", "/ecom/medias/sys_master/public/9375747014686/C-0026-small-60x60.png", 8],
+
+            # Espresso
+            ["Livanto", "/ecom/medias/sys_master/public/9375746097182/C-0017-small-60x60.png", 6],
+            ["Capriccio", "/ecom/medias/sys_master/public/9375746097182/C-0017-small-60x60.png", 5],
+            ["Volluto", "/ecom/medias/sys_master/public/9375748522014/C-0039-small-60x60.png", 4],
+            ["Cosi", "/ecom/medias/sys_master/public/9740977668126/Cosi-small-60x60.png", 4],
+
+            # Pure origin
+            ["Indriya from India", "/ecom/medias/sys_master/public/9375745572894/C-0015-small-60x60.png", 10],
+            ["Rosabaya de Colombia", "/ecom/medias/sys_master/public/9375747538974/C-0027-small-60x60.png", 6],
+            ["Dulsão do Brasil", "/ecom/medias/sys_master/public/9375744655390/C-0009-small-60x60.png", 4],
+            ["Bukeela ka Ethiopia", "/ecom/medias/sys_master/public/9375752126494/C-0103-small-60x60.png", 3],
+
+            # Lungo
+            ["Fortissio Lungo", "/ecom/medias/sys_master/public/9381719441438/C-0126-small-60x60.png", 8],
+            ["Vivalto Lungo", "/ecom/medias/sys_master/public/9375748063262/C-0038-small-60x60.png", 4],
+            ["Linizio Lungo", "/ecom/medias/sys_master/public/9375749046302/C-0057-small-60x60.png", 4],
+
+            # Dacaffeinato
+            ["Arpeggio Decaffeinato", "/ecom/medias/sys_master/public/9559895605278/ArpeggioDecaffeinato-small-60x60.png", 9],
+            ["Volluto Decaffeinato", "/ecom/medias/sys_master/public/9559893999646/VollutoDecaffeinato-small-60x60.png", 4],
+            ["Vivalto Lungo Decaffeinato", "/ecom/medias/sys_master/public/9559900717086/VivaltoDecaffeinato-small-60x60.png", 4],
+        ];
+
+        foreach ($items as $key => $item) {
+            list($name, $uri) = $item;
+            $key++;
+            $this->_db->query("INSERT INTO item (name, image) VALUES ('$name', 'https://www.nespresso.com$uri');");
+        }
     }
 
     public function onEvent($args)
@@ -74,9 +120,6 @@ class Backend
                     break;
             }
         }
-
-//        echo "on event:\n";
-//        var_dump($args);
     }
 
     public function doList($data)
